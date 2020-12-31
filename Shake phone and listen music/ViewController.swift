0dd152a60785b667b8fd26f70ada8a40df44cdbb
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-31 12:32:48 +0200

    Initial Commit

---
+++ Shake phone and listen music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shake phone and listen music
+//
+//  Created by Сергей Голенко on 31.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
